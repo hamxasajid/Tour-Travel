42bc96beac54f831e0854271c4513a1bed0b880e
hamxasajid <hamzasajid4341@gmail.com>
2023-12-10 12:48:52 +0500

    Your commit message here

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+const menuIcon = document.querySelector('.menu-icon');
+const navList = document.querySelector('.list');
+
+menuIcon.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+/////////////////////Slider///////////////////
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("myslide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
